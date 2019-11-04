94bd32294d322a0e4ebe811dce66823a1e80cbd8
Andrija Buzinac <andrija.buzinac@gmail.com>
2019-11-04 23:29:44 +0100

    index.php
    main page styling

--- register.php
+++ register.php
@@ -25,7 +25,7 @@ function getInputValue($name)
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome to musixbox!</title>
+    <title>Welcome to Musixbox!</title>
 
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Bree+Serif&display=swap" rel="stylesheet">
